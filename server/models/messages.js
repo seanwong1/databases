c142fc71e6af8f1734119721e6d2aea93e391043
Lovinson Dieujuste <lovinsondieujuste@Lovinsons-MacBook-Pro.local>
2023-01-21 13:49:48 -0500

    Complete customizing tests

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,16 @@
 var db = require('../db');
+//var connection = db.dbConnection;
 
+// connection.query('insert into users (username) values('jake'), ('josh'), ('jack')')
 module.exports = {
   getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  create: function () {
+    // var message = req.body;
+    // console.log(message);
+    //connection.query(`INSERT INTO messages (${message})`
+    // connection.execute('select 1 + ? + ? as result', [5, 6], (err, rows) => {
+    //   // rows: [ { result: 12 } ]
+    //   // internally 'select 1 + ? + ? as result' is prepared first. On subsequent calls cached statement is re-used
+    // });
+  } // a function which can be used to insert a message into the database
 };
